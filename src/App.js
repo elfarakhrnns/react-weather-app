16d5e1f8e8b9e5ded3480210eb7880f090642a65
elfarakhrnns <elfara.khrnns@gmail.com>
2023-11-13 14:58:02 +0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://inspiring-puppy-8608c9.netlify.app" target="_blank">
-          Elfara Khairunnisa
-        </a>{" "}
-        and is open-sourced in{" "}
-        <a
-          href="https://github.com/elfarakhrnns/react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://inspiring-puppy-8608c9.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Elfara Khairunnisa
+          </a>{" "}
+          and is open-sourced in{" "}
+          <a
+            href="https://github.com/elfarakhrnns/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
