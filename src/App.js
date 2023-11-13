b7afca86f28538a66f80e70e36557c9f074ef37c
elfarakhrnns <elfara.khrnns@gmail.com>
2023-11-13 14:39:44 +0700

    updated the link to github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Elfara Khairunnisa and is open sourced in{" "}
-        <a href="https://github.com/elfarakhrnns/react-weather-app">Github</a>.
+        <a
+          href="https://github.com/elfarakhrnns/react-weather-app"
+          target="_blank"
+        >
+          Github
+        </a>
+        .
       </footer>
     </div>
   );
