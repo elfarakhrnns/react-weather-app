e97b62c7fd3ac99abd7cd3651eda10464d405020
elfarakhrnns <elfara.khrnns@gmail.com>
2023-11-13 14:38:40 +0700

    added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Elfara Khairunnisa and is open sourced in{" "}
+        <a href="https://github.com/elfarakhrnns/react-weather-app">Github</a>.
+      </footer>
+    </div>
+  );
 }
