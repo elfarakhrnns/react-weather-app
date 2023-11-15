fbb8b41c94fd5e143f3b196b2918ed04d642e099
elfarakhrnns <elfara.khrnns@gmail.com>
2023-11-15 14:18:28 +0700

    updated the weather css

---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
+      <h1>Tokyo</h1>
+      <ul>
+        <li>Wednesday 11:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Cloudy"
+            className="float-left"
+          />
+          <span className="temperature">14</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 40%</li>
+            <li>Humidity: 62%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
